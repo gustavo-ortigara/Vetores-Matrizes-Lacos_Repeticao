c3b8d982fb2c26dbf9e27a7a9a2863cd80d6bd4d
gustavo-ortigara <gustavo.ortigara@gmail.com>
2025-09-21 14:16:32 -0300

    Essa atividade exibe uma matriz identidade de 5x5. Quando a linha e coluna tiverem a mesma posicao (por exemplo linha 2 e coluna 2), recebem o valor 1, caso contrario, recebem o valor 0.

--- src/Atividade_Desaf01.java
+++ src/Atividade_Desaf01.java
@@ -5,7 +5,6 @@ public class Atividade_Desaf01 {
         int coluna = 5;
         int[][] matriz = new int[linha][coluna];
 
-        // Preencher a matriz identidade
         for (int i = 0; i < linha; i++) {
             for (int j = 0; j < coluna; j++) {
                 if (i == j) {
@@ -16,7 +15,6 @@ public class Atividade_Desaf01 {
             }
         }
 
-        // Exibir a matriz
         System.out.println("Matriz Identidade 5x5:");
         for (int i = 0; i < linha; i++) {
             for (int j = 0; j < coluna; j++) {
